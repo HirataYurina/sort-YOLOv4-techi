18d325ce509ad162a9258ba6b73d69a415d7bbdb
RuiLi <1353593259@qq.com>
2020-09-18 08:11:09 +0800

    commit

--- detector.py
+++ detector.py
@@ -4,3 +4,24 @@
 # datetime:1993/12/01
 # filename:detector.py
 # software: PyCharm
+
+import tensorflow as tf
+
+
+class Detector:
+
+    def __init__(self, model_path):
+        self.model_path = model_path
+        self.model = self.generate_model()
+
+    def generate_model(self):
+        # create model
+        model = tf.saved_model.load(self.model_path)
+        model = model.signatures['serving_default']
+        return model
+
+    def detect(self, inputs, image_shape):
+        # inference
+        boxes, scores, classes = self.model(input_2=inputs,
+                                            input_3=image_shape)
+        return boxes, scores, classes
--- kalman_filter.py
+++ kalman_filter.py
@@ -4,3 +4,21 @@
 # datetime:1993/12/01
 # filename:kalman_filter.py
 # software: PyCharm
+
+import numpy as np
+import scipy
+
+
+class KalmanFilter:
+
+    def __init__(self):
+        pass
+
+    def predict(self):
+        pass
+
+    def update(self):
+        pass
+
+    def maha_distance(self):
+        pass
